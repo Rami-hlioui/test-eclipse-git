85ec2c2e0e168b2b5e8872034810da5e0538ab29
Rami-hlioui <ramihlioui.tsuna@gmail.com>
2022-09-27 15:07:41 +0100

    hedhy commit okhra

--- src/main/java/testGit/wevioo/App.java
+++ src/main/java/testGit/wevioo/App.java
@@ -8,7 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-    	
+    	System.out.println( "waaaaa" );
+    	System.out.println( "aaaa" );
+    	System.out.println( "message 2525" );
+
     	System.out.println( "katous rose" );
         System.out.println( "katous a7mer" );
         System.out.println( "katous akhdher" );
