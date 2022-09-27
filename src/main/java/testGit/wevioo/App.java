16af178167c0ac0f49e32f79fccc255bcb895535
Rami-hlioui <ramihlioui.tsuna@gmail.com>
2022-09-27 15:02:13 +0100

    heeee

--- src/main/java/testGit/wevioo/App.java
+++ src/main/java/testGit/wevioo/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+    	
     	System.out.println( "katous rose" );
         System.out.println( "katous a7mer" );
         System.out.println( "katous akhdher" );
